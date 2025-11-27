de3e2b7006aca261ace25254c233a28f9cb9472b
hartlacuarin <hartkristianeclacuarin@iskolarngbayan.pup.edu.ph>
2025-11-27 11:43:22 +0800

    add data.js

---
+++ data.js
@@ -0,0 +1,140 @@
+const empires = [
+    {
+        "name": "Order Empire",
+        "image": "assets/order_empire.jpg",
+        "units": [
+            {
+                "unitName": "Swordwrath",
+                "description": "The basic, fast-moving melee unit of the Order Empire.",
+                "image": "assets/unit_swordwrath.jpg"
+            },
+            {
+                "unitName": "Spearton",
+                "description": "A versatile defensive unit known for their shield and spear.",
+                "image": "assets/unit_spearton.jpg"
+            },
+            {
+                "unitName": "Shadowrath",
+                "description": "A stealth unit that specializes in rapid infiltration and assassination.",
+                "image": "assets/unit_shadowrath.jpg"
+            },
+            {
+                "unitName": "Archidon",
+                "description": "A powerful ranged unit, specializing in arrow fire.",
+                "image": "assets/unit_archidon.jpg"
+            },
+            {
+                "unitName": "Albowtross",
+                "description": "A specialized flying or long-range siege unit.",
+                "image": "assets/unit_albowtross.jpg"
+            },
+            {
+                "unitName": "Meric",
+                "description": "A unique support unit, focused on healing or unique crowd control.",
+                "image": "assets/unit_meric.jpg"
+            },
+            {
+                "unitName": "Magikill",
+                "description": "A support unit that casts magical spells to aid allies and hinder enemies.",
+                "image": "assets/unit_magikill.jpg"
+            },
+            {
+                "unitName": "Enslaved Giant",
+                "description": "The massive, heavy unit used for breaking through defenses and absorbing damage.",
+                "image": "assets/unit_enslaved_giant.jpg"
+            }
+        ]
+    },
+    {
+        "name": "Chaos Empire",
+        "image": "assets/chaos_empire.jpg",
+        "units": [
+            {
+                "unitName": "Crawler",
+                "description": "Fast-moving, expendable melee unit often used to overwhelm enemy lines.",
+                "image": "assets/unit_crawler.jpg"
+            },
+            {
+                "unitName": "Bomber",
+                "description": "Suicide or siege unit that deals massive area damage upon impact or explosion.",
+                "image": "assets/unit_bomber.jpg"
+            },
+            {
+                "unitName": "Juggerknight",
+                "description": "Heavy, durable melee unit designed to absorb large amounts of damage.",
+                "image": "assets/unit_juggerknight.jpg"
+            },
+            {
+                "unitName": "Dead",
+                "description": "An undead unit, possibly a resurgent fighter with unique properties like disease or debuffs.",
+                "image": "assets/unit_dead.jpg"
+            },
+            {
+                "unitName": "Eclipsor",
+                "description": "A ranged unit that fires darkness or chaos-infused projectiles.",
+                "image": "assets/unit_eclipsor.jpg"
+            },
+            {
+                "unitName": "Marrowkai",
+                "description": "A specialized unit, often linked to bone, necromancy, or heavy piercing damage.",
+                "image": "assets/unit_marrowkai.jpg"
+            },
+            {
+                "unitName": "Medusa",
+                "description": "A powerful ranged or support unit with an ability to stun or turn units to stone.",
+                "image": "assets/unit_medusa.jpg"
+            },
+            {
+                "unitName": "Chaos Giant",
+                "description": "The ultimate heavy unit for the Chaos Empire, inflicting massive damage and destruction.",
+                "image": "assets/unit_chaos_giant.jpg"
+            }
+        ]
+    },
+    {
+        "name": "Elemental Empire",
+        "image": "assets/elemental_empire.jpg",
+        "units": [
+            {
+                "unitName": "Water Elemental",
+                "description": "Units specializing in defensive and crowd-control abilities.",
+                "image": "assets/unit_water.jpg"
+            },
+            {
+                "unitName": "Fire Elemental",
+                "description": "Aggressive units that deal burning damage over time.",
+                "image": "assets/unit_fire.jpg"
+            },
+            {
+                "unitName": "Earth Elemental",
+                "description": "Heavy, durable units that focus on ground defense and shockwaves.",
+                "image": "assets/unit_earth.jpg"
+            },
+            {
+                "unitName": "Air Elemental",
+                "description": "Fast units with aerial advantages and high mobility.",
+                "image": "assets/unit_air.jpg"
+            },
+            {
+                "unitName": "Charrog",
+                "description": "A specialized fire unit, likely a fusion or powerful variant.",
+                "image": "assets/unit_charrog.jpg"
+            },
+            {
+                "unitName": "Treature",
+                "description": "A siege or support unit associated with the Earth Elementals.",
+                "image": "assets/unit_treature.jpg"
+            },
+            {
+                "unitName": "Cycloid",
+                "description": "An aerial support mage unit in the Elemental army, primarily used for eliminating flying enemies and stunning groups of ground units.",
+                "image": "assets/unit_cycloid.jpg"
+            },
+            {
+                "unitName": "V",
+                "description": "The powerful entity and leader of the Elemental Empire.",
+                "image": "assets/unit_v.jpg"
+            }
+        ]
+    }
+];
